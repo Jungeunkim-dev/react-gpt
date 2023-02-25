3e37e9f5d9a978a08898d28a0ffbb4cff97801df
Jungeunkim-dev <svaha@ewhain.net>
2023-02-25 16:48:32 +0900

    update changes

---
+++ src/pages/goodDay.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function goodDay() {
+  const [userInput, setUserInput] = useState("")
+  const [result, setResult] = useState("")
+
+  useEffect(() => {
+    console.log('rendered..')
+    console.log(userInput)
+  }, [userInput])
+
+
+  const submit = async () => {
+    const url = "/api/hello"
+    const res = await axios.post(url, {
+      userInput: userInput
+    })
+    console.log(res.data.result)
+    setResult(res.data.result)
+    return true
+  }
+
+  return <div>
+
+    <input onChange={e => setUserInput(e.target.value)}></input>
+    <button onClick={e => submit()}>send</button>
+    <div>
+      {result}
+    </div>
+
+  </div>
+}
---
+++ src/pages/users/signin.js
@@ -0,0 +1,9 @@
+
+export default function Signin() {
+  return <div>Signin
+
+    <button>aaa</button>
+    <input></input>
+  </div>
+}
+
